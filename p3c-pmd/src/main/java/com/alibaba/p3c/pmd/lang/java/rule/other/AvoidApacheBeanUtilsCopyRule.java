27500f9190eec9bb014fdf57a7807ee5e3abbbb5
atuowgo <atuowgo@outlook.com>
2019-10-13 21:52:52 +0800

    support check org.apache.commons.beanutils.BeanUtils.copyProperties

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/other/AvoidApacheBeanUtilsCopyRule.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/other/AvoidApacheBeanUtilsCopyRule.java
@@ -30,8 +30,9 @@ import net.sourceforge.pmd.lang.ast.Node;
  */
 public class AvoidApacheBeanUtilsCopyRule extends AbstractXpathRule {
     private static final String XPATH =
-            "//PrimaryPrefix/Name[@Image='BeanUtils.copyProperties' and "
-            + "//ImportDeclaration[@ImportedName='org.apache.commons.beanutils.BeanUtils']]";
+            "//PrimaryPrefix/Name[(@Image='BeanUtils.copyProperties' and "
+                + "//ImportDeclaration[@ImportedName='org.apache.commons.beanutils.BeanUtils']) or "
+                + "starts-with(@Image,'org.apache.commons.beanutils.BeanUtils.')]";
 
     public AvoidApacheBeanUtilsCopyRule() {
         setXPath(XPATH);

3402c1da4c98fc300da3ce334037ac1330d518ac
XenoAmess <xenoamess@gmail.com>
2020-04-11 06:22:53 +0800

    update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,17 +1,17 @@
 cd ./p3c-pmd
-call mvn clean install -Dmaven.javadoc.skip=false
+call mvn clean install -Dmaven.javadoc.skip=false -e
 cd ../
 cd ./idea-plugin
 
 cd ./p3c-common
-call gradle publishToMavenLocal
+call ../gradlew publishToMavenLocal
 cd ../
 
 cd ./p3c-idea
-call gradle publishToMavenLocal
+call ../gradlew publishToMavenLocal
 cd ../
 
-call gradle buildPlugin
+call ./gradlew buildPlugin
 
 cd ../
 pause

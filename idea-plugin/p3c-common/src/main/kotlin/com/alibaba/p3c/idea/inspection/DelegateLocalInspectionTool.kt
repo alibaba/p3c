3b27447a197dee9f4806f2636f97aff8dc81f64a
XenoAmess <xenoamess@gmail.com>
2020-08-23 16:00:43 +0800

    fix bug in kotlin. add version tag in plugin.xml.

--- idea-plugin/p3c-common/src/main/kotlin/com/alibaba/p3c/idea/inspection/DelegateLocalInspectionTool.kt
+++ idea-plugin/p3c-common/src/main/kotlin/com/alibaba/p3c/idea/inspection/DelegateLocalInspectionTool.kt
@@ -49,7 +49,7 @@ class DelegateLocalInspectionTool : LocalInspectionTool(), AliBaseInspection {
             file: PsiFile,
             manager: InspectionManager,
             isOnTheFly: Boolean
-    ): Array<ProblemDescriptor>? {
+    ): Array<ProblemDescriptor?>? {
         return localInspectionTool.checkFile(file, manager, isOnTheFly)
     }
 
